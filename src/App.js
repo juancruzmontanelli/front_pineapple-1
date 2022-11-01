384ef9462bfd01da45d5773493dd55f6ec7b81cf
patricioimbrogno <patricioimbrogno@gmail.com>
2022-11-01 19:13:34 -0300

    contenido mergeado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./containers/Home"
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
       <Footer />
     </>
   );
