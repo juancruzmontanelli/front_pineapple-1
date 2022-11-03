820762656c9b19426a72e94fddec12c8d8e59166
lucianocasini <lucianoo.casini@gmail.com>
2022-11-03 18:19:55 -0300

    Creacion de reducer carrito

---
+++ src/state/cart.js
@@ -0,0 +1,34 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+export const setCart = createAction("SET_CART");
+export const addProduct = createAction("ADD_PRODUCT");
+export const removeProduct = createAction("REMOVE_PRODUCT");
+export const incrementQuantity = createAction("INCREMENT_QUANTITY");
+export const decrementQuantity = createAction("DECREMENT_QUANTITY");
+
+export default createReducer([], {
+  [setCart]: (state, action) => {
+    return state;
+  },
+  [addProduct]: (state, action) => {
+    if (state.find((prod) => prod.id === action.payload.id)) {
+      return state;
+    }
+    return [...state, { ...action.payload, quantity: 1 }];
+  },
+  [removeProduct]: (state, action) => {
+    return state.filter((prod) => prod.id !== action.payload.id);
+  },
+  [incrementQuantity]: (state, action) => {
+    const item = state.find((prod) => prod.id === action.payload);
+    item.quantity++;
+  },
+  [decrementQuantity]: (state, action) => {
+    const item = state.find((prod) => prod.id === action.payload);
+    if (item.quantity === 1) {
+      item.quantity = 1;
+    } else {
+      item.quantity--;
+    }
+  },
+});
--- src/state/store.js
+++ src/state/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './user'
+import userReducer from "./user";
+import cartReducer from "./cart";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
