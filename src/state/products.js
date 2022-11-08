ea433d8ddf5a11ea204d2a52b0e895ac8c25a8c8
patricioimbrogno <patricioimbrogno@gmail.com>
2022-11-08 15:29:51 -0300

    search/filters

--- src/state/products.js
+++ src/state/products.js
@@ -2,10 +2,14 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const setProducts = createAction("SET_PRODUCTS");
 export const searchProducts = createAction("SEARCH_PRODUCTS");
+export const filterBrandProducts = createAction("FILTER_BRAND");
+export const filterPriceProducts = createAction("FILTER_PRICE");
 
-const initialState = {products: [], search: []}
+const initialState = {products: [], search: [], filterBrand: [], filterPrice: []}
 
 export default createReducer(initialState, {
   [setProducts]: (state, action) => {return {...state, products: action.payload }},
-  [searchProducts]: (state, action) => {return {...state, search: action.payload }}
+  [searchProducts]: (state, action) => {return {...state, search: action.payload }},
+  [filterBrandProducts]: (state, action) => {return {...state, filterBrand: action.payload }},
+  [filterPriceProducts]: (state, action) => {return {...state, filterPrice: action.payload }}
 });
