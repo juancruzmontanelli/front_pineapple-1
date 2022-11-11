a673f4b9c09e40845f8c3808f4634b55e717eb96
lucianocasini <lucianoo.casini@gmail.com>
2022-11-11 13:38:36 -0300

    Historial de compras admin dinamico al cambiar status

--- src/components/AdminMenuOrders.js
+++ src/components/AdminMenuOrders.js
@@ -1,68 +1,71 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import axios from 'axios'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import axios from "axios";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const StyledMenu = styled((props) => (
   <Menu
     elevation={0}
     anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
+      vertical: "bottom",
+      horizontal: "right",
     }}
     transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
+      vertical: "top",
+      horizontal: "right",
     }}
     {...props}
   />
 ))(({ theme }) => ({
-  '& .MuiPaper-root': {
+  "& .MuiPaper-root": {
     borderRadius: 6,
     marginTop: theme.spacing(1),
     minWidth: 180,
     color:
-      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
     boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
     },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
         fontSize: 18,
         color: theme.palette.text.secondary,
         marginRight: theme.spacing(1.5),
       },
-      '&:active': {
+      "&:active": {
         backgroundColor: alpha(
           theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
+          theme.palette.action.selectedOpacity
         ),
       },
     },
   },
 }));
 
-export default function AdminMenuOrders( {stat, id }) {
+export default function AdminMenuOrders({ stat, id, setModified }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     axios
-        .put(`/api/cart/history/update/${id}`, { status:e.target.innerText })
-        .then(() => {
-        })
-        .catch((error) => {
-          console.log(error);
-        })
+      .put(`/api/cart/history/update/${id}`, { status: e.target.innerText })
+      .then((res) => {
+        setModified(res.data.id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     setAnchorEl(null);
   };
 
@@ -70,9 +73,9 @@ export default function AdminMenuOrders( {stat, id }) {
     <div>
       <Button
         id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
+        aria-controls={open ? "demo-customized-menu" : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
+        aria-expanded={open ? "true" : undefined}
         variant="contained"
         disableElevation
         onClick={handleClick}
@@ -83,22 +86,21 @@ export default function AdminMenuOrders( {stat, id }) {
       <StyledMenu
         id="demo-customized-menu"
         MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
+          "aria-labelledby": "demo-customized-button",
         }}
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
       >
-        <MenuItem value='Pendiente' onClick={handleClose} disableRipple>
+        <MenuItem value="Pendiente" onClick={handleClose} disableRipple>
           Pendiente
         </MenuItem>
-        <MenuItem value='Confirmada' onClick={handleClose} disableRipple>
+        <MenuItem value="Confirmada" onClick={handleClose} disableRipple>
           Confirmada
         </MenuItem>
-        <MenuItem value='Rechazada' onClick={handleClose} disableRipple>
+        <MenuItem value="Rechazada" onClick={handleClose} disableRipple>
           Rechazada
         </MenuItem>
-
       </StyledMenu>
     </div>
   );
--- src/components/AdminOrders.js
+++ src/components/AdminOrders.js
@@ -10,24 +10,24 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
-import AdminMenuOrders from './AdminMenuOrders'
-import AdminMenuProducts from './AdminMenuProducts'
-import axios from 'axios'
+import AdminMenuOrders from "./AdminMenuOrders";
+import AdminMenuProducts from "./AdminMenuProducts";
+import axios from "axios";
 
 const AdminOrders = () => {
-
   const [orders, setOrders] = useState([]);
+  const [modified, setModified] = useState({});
 
   useEffect(() => {
     axios
       .get(`/api/cart/history`)
       .then((res) => {
-       setOrders(res.data);
+        setOrders(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [modified]);
 
   return (
     <Grid
@@ -46,8 +46,8 @@ const AdminOrders = () => {
           <Table aria-label="simple table">
             <TableHead>
               <TableRow>
-              <TableCell align="left">ID de usuario</TableCell>
-              <TableCell align="left">ID de orden</TableCell>
+                <TableCell align="left">ID de usuario</TableCell>
+                <TableCell align="left">ID de orden</TableCell>
                 <TableCell align="left">Productos</TableCell>
                 <TableCell align="left">Status</TableCell>
               </TableRow>
@@ -55,20 +55,23 @@ const AdminOrders = () => {
             <TableBody>
               {orders.map((order) => (
                 <TableRow
-              
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
-                  {order.orderItems[0].userId}
+                    {order.orderItems[0].userId}
                   </TableCell>
                   <TableCell component="th" scope="row">
-                  {order.id}
+                    {order.id}
                   </TableCell>
                   <TableCell component="th" scope="row">
-                  <AdminMenuProducts products={order.orderItems} />
+                    <AdminMenuProducts products={order.orderItems} />
                   </TableCell>
                   <TableCell component="th" scope="row">
-                  <AdminMenuOrders stat={order.status} id={order.id}/> 
+                    <AdminMenuOrders
+                      stat={order.status}
+                      id={order.id}
+                      setModified={setModified}
+                    />
                   </TableCell>
                 </TableRow>
               ))}
